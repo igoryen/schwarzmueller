074e99476688c57cc37ffd465f7f5188593dd95b
Igor Entaltsev <igor.yentaltsev@gmail.com>
2019-09-24 17:18:30 -0400

    17. Listening to events

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 new Vue({
     el: "#app",
     data: {
+        counter: 0,
         title: "Hello World!",
         link: "http://youtube.com",
         finishedLink: '<a href="http://google.com">Google</a>'
     },
     methods: {
+        increase: function(){
+            this.counter++;
+        },
         sayHello: function(){
             this.title = "Hola!";
             return this.title;
