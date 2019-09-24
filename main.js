c34419c8fca001236d67da1e1173832920e1b523
Igor Entaltsev <igor.yentaltsev@gmail.com>
2019-09-24 17:10:03 -0400

    15. How to Output Raw HTML

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ new Vue({
     el: "#app",
     data: {
         title: "Hello World!",
-        link: "http://youtube.com"
+        link: "http://youtube.com",
+        finishedLink: '<a href="http://google.com">Google</a>'
     },
     methods: {
         sayHello: function(){
