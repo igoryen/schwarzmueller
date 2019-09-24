5f2e5805cea05fbad8d43bff833e528552fa34e5
Igor Entaltsev <igor.yentaltsev@gmail.com>
2019-09-24 15:46:17 -0400

    rendering a data property

--- main.js
+++ main.js
@@ -2,10 +2,5 @@ new Vue({
     el: "#app",
     data: {
         title: "Hello World!"
-    },
-    methods: {
-        changeTitle: function( event ){
-            this.title = event.target.value;
-        }
     }
 });
