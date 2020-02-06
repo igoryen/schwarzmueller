1faa2468c503798367031ffb5d4b8570c146a08e
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-02-05 20:45:53 -0500

    263. Using the centralized state (Section code (Start))

--- ex03/New folder/Section-Code-Start/Section Code (Start)/src/App.vue
+++ ex03/New folder/Section-Code-Start/Section Code (Start)/src/App.vue
@@ -4,7 +4,7 @@
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Vuex</h1>
                 <app-result :counter="counter"></app-result>
-                <hr />
+                <hr>
                 <app-counter @updated="counter += $event"></app-counter>
             </div>
         </div>
@@ -12,19 +12,19 @@
 </template>
 
 <script>
-import Counter from "./components/Counter.vue";
-import Result from "./components/Result.vue";
+    import Counter from './components/Counter.vue';
+    import Result from './components/Result.vue';
 
-export default {
-    data() {
-        return {
-            counter: 0
-        };
-    },
-    components: {
-        appCounter: Counter,
-        appResult: Result
+    export default {
+        data() {
+            return {
+                counter: 0
+            }
+        },
+        components: {
+            appCounter: Counter,
+            appResult: Result,
+        }
     }
-};
 </script>
 
